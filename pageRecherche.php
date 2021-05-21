99e5e2d5b1efd8ead2d092f0307f54b581721d35
Managatrix <mana.tomfd@gmail.com>
2021-05-21 15:11:44 +0200

    Added DB connexion and results printing

--- pageRecherche.php
+++ pageRecherche.php
@@ -66,19 +66,40 @@
                         <tr>
                             <table class="resultats">
                                 <tr>
-                                    <td><a href=""><img src="" alt="Jeu11"></a></td>
-                                    <td><a href=""><img src="" alt="Jeu12"></a></td>
-                                    <td><a href=""><img src="" alt="Jeu13"></a></td>
-                                </tr>
-                                <tr>
-                                    <td><a href=""><img src="" alt="Jeu21"></a></td>
-                                    <td><a href=""><img src="" alt="Jeu22"></a></td>
-                                    <td><a href=""><img src="" alt="Jeu23"></a></td>
-                                </tr>
-                                <tr>
-                                    <td><a href=""><img src="" alt="Jeu31"></a></td>
-                                    <td><a href=""><img src="" alt="Jeu32"></a></td>
-                                    <td><a href=""><img src="" alt="Jeu33"></a></td>
+                                <?php
+                                //paramètres de connexion à la base de données
+                                $Server = "localhost";
+                                $User = "root";
+                                $Pwd = "";
+                                $DB = "projet-ludotheque";
+                                //connexion au serveur où se trouve la base de données
+                                $Connect = mysqli_connect($Server, $User, $Pwd, $DB);
+                                if (!$Connect) {
+                                    echo "Connexion à la base impossible";
+                                }
+                                //Ecriture de la requête
+                                $Query = "SELECT * FROM game";
+                                //Envoi de la requête
+                                $Result = $Connect->query($Query);
+
+                                $nbColonnes = 0;
+                                $nbLignes = 0;
+                                $nbColonnesMax = 2;
+                                while($Data = mysqli_fetch_array($Result))
+                                {
+                                    if ($nbColonnes < $nbColonnesMax)
+                                    {
+                                        echo "
+                                        <td><a href=''><img class='jeu' src='$Data[6]' alt='$Data[1]'></a></td>";
+                                        $nbColonnes ++;
+                                    }
+                                    else
+                                    {
+                                        echo "</tr><td><a href=''><img class='jeu' src='$Data[6]' alt='$Data[1]'></a></td><tr>";
+                                        $nbColonnes = 0;
+                                    }
+                                }
+                                ?>
                                 </tr>
                             </table>
                         </tr>
@@ -87,6 +108,14 @@
             </td>
         </tr>
     </table>
+    <?php
+    //Traitement de la réponse
+    while($Data = mysqli_fetch_array($Result))
+    {
+        echo "IDGame : $Data[0], Name : $Data[1], AgeMin : $Data[2], AgeMax : $Data[3], Type : $Data[4], Abstract : $Data[5], ImagePath : $Data[6]</br></br>";
+    }
+    mysqli_close($Connect);
+    ?>
 </body>
 
 </html>
